3a635eafff83de6570cdddbc85fb8bffc5250249
bigkillroy <amb429@cornell.edu>
2014-05-14 17:10:57 -0400

    fixed contact form bug
    quick fix

--- Logic/contactForm.php
+++ Logic/contactForm.php
@@ -12,23 +12,28 @@ function collectInputs() {
 	$success = true;
 	if ( isset($_POST["from"]) && validateText($_POST["from"]) ){
 		$from = $_POST["from"];
-	} else $success = false;
+	} else{
+		$success = false;
+	}
 	if ( isset($_POST["email"]) && validateEmail($_POST["email"]) ){
 		$email = $_POST["email"];
-	} else $success = false;
+	} else{
+		$success = false;
+	}
 	$category = $_POST['category'];
 	if ( isset($_POST["subject"]) && validateText($_POST["subject"]) 
 			&& isset($_POST["message"]) && validateText($_POST["message"])){
 		$subject = "From contact form: ".$category.$_POST["subject"];
 		$message = $from." says: ".$_POST["message"];
-	} else $success = false;
+	} else {
+	$success = false;
+	}
 	if($success) {
 		$success = mail("kfm53@cornell.edu", $subject, $message, "From: $email");
 	}
     if($success) {
        echo "<p>Thank you for reaching out! Sabor Latino will try to respond to your inquiry as soon as possible.</p>";
-    }
-    else {
+    }else {
         echo "<p>Oh no! Looks like something went wrong. Please try again.</p>";
     }
 	
